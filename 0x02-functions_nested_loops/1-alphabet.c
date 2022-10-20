35776f352a7ba6f1be6723e0d1e535db6e6ac85a
ogbkachi <ogbkachi@gmail.comm>
2022-10-19 19:12:13 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,10 +6,10 @@
 
 void print_alphabet(void)
 {
-	char letter;
+	int i;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
 
 		_putchar('\n');
 }
